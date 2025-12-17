df7fd50841dacec4076c9fb066ef26b03a2c679d
analu0905 <115155605+analu0905@users.noreply.github.com>
2025-12-17 17:03:27 -0300

    Update FenixAcademia.java

--- FenixAcademia.java
+++ FenixAcademia.java
@@ -1,11 +1,60 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class FenixAcademia {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        List<Aluno> alunos = new ArrayList<>();
+        List<Instrutor> Instrutores = new ArrayList<>();
+        List<Atividade> Atividades = new ArrayList<>();
 
-        System.out.println("Bem-vindo à Fênix Academia!");
+        int opcao;
+
+        System.out.println("\n    -------Bem-vindo à Fênix Academia!-------");
+        System.out.println("Digite a opção");
+        System.out.println("1- CADASTRAR ALUNO");
+        System.out.println("2- CADASTRAR INSTRUTOR");
+        System.out.println("3- CRIAR ATIVIDADE");
+        System.out.println("4- MATRICULAR ALUNO EM ATIVIDADE");
+        System.out.println("5- PAGAR MENSALIDADE");
+        System.out.println("6- LISTAR ALUNOS");
+        System.out.println("0- SAIR");
+        opcao = sc.nextInt();
+
+        switch(opcao) {
+            case 1:
+                cadastrarAluno();
+                break;
+            case 2:
+                cadastrarInstrutor();
+                break;
+            case 3:
+                criarAtividade();
+                break;
+            case 4:
+                matricularAluno();
+                break;
+            case 5:
+                pagarMensalidade();
+                break;
+            case 6:
+                listarAlunos();
+                break;
+            case 7:
+                listarAtividades();
+                break;
+            case 0:
+                System.out.println("Encerrando sistema...");
+                break;
+            default:
+                System.out.ptintln("Opção inválida");
+        }
 
 
     }
 }
+
+
+
+
+
